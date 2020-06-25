4ca69250de1b080023f45c238e41c474e955c8f9
untalinfo <racso1607@gmail.com>
2020-06-25 19:49:52 +0000

    fix docstring and file init added

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,47 @@
 #!/usr/bin/python3
+"""
+Module Classe base for airbnb
+"""
 import uuid
 import datetime
 
 
 class BaseModel:
+    """
+    This class defines attributes or methods for
+    other classes
+    """
     name = None
     my_number = 0
-    id = None
 
     def __init__(self):
+        """
+        Initialize of base model class
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """
+        crate a string
+
+        Returns:
+            [str]: [Unofficial string]
+        """
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Update the public instance updated_at to current
+        """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """
+        Generate a dictionary of the class
+        """
         dic = {}
         dic["__class__"] = str(type(self).__name__)
         dic['id'] = self.id

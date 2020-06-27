f5437c2d30c076aaf8e75dbf765fab265abfd075
somarae8 <1493@holbertonschool.com>
2020-06-27 02:51:03 +0000

    add classes

---
+++ amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Amenity class """
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class Amenity that inherits from BaseModel"""
+
+    name = ""
